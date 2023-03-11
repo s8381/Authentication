aeb3b53219696492eb90008adffd2ca3ecbb7586
tpircsnoitcA <s8381@DESKTOP-24UMPVF.localdomain>
2023-03-11 18:17:29 +0800

    add login controller, login page and its dependencies

---
+++ src/main/java/springboot/authentication/controller/MemberAccountController.java
@@ -0,0 +1,14 @@
+package springboot.authentication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import springboot.authentication.entity.MemberAccount;
+
+@Controller
+public class MemberAccountController {
+    @GetMapping("/login")
+    public String login(@ModelAttribute MemberAccount memberAccount){
+        return "login";
+    }
+}
